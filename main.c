47d4f11961c4bef48fba42ead50b688aec358185
Dufour <adufour@ifa501-e01.insa-lyon.fr>
2020-02-24 17:15:48 +0100

    main avec boucle

--- main.c
+++ main.c
@@ -6,23 +6,30 @@ int main()
   SMAT *H;
   VEC  *r;
 
-  fp = fopen( "exemple2.dat", "r" );
+  fp = fopen( "exemple.dat", "r" );
   H = sm_input( fp );
   fclose( fp );
   sm_output( stdout, H );
   sm_convert(H);
   sm_output( stdout, H );
 
-  r = v_get(3);
-  r->e[0] = 1./3;
-  r->e[1] = 1./3;
-  r->e[2] = 1./3;
+  r = v_get(H->m);
+  for (int i = 0; i < H->m; i++)
+  {
+  	r->e[i] = 1. / H->m;
+  }
 
   v_output(stdout, r);
 
-  printf("multiplication :\n");
-  r = product_vm(r, H);
-  v_output(stdout, r);
+  for (int i = 0; i < 100; i++)
+  {
+  	printf("itération %d:\n", i);
+	r = product_vm(r, H);
+	v_output(stdout, r);
+  }
+
+  sm_free(H);
+  v_free(r);
 
   return 0;
 }
